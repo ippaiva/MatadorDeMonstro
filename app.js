2ac5be77970a1b46131d88201cb67da8f88136a3
Ismael Paiva <i-paiva@hotmail.com>
2019-02-08 15:20:04 -0200

    almost done

--- app.js
+++ app.js
@@ -17,17 +17,30 @@ new Vue({
 			this.monsterLife = 100;
 		},
 		attack(especial) {
-			this.hurt(7, 12, false);
+			this.hurt('playerLife', 7, 12, false);
+			this.hurt('monsterLife', 5, 10, especial);
 		},
-		hurt(min, max, especial) {
+		hurt(prop, min, max, especial) {
 			const plus = especial ? 5 : 0;
 			const hurt = this.getRandom(min + plus, max + plus);
-			this.playerLife = Math.max(this.playerLife - hurt, 0);
+			this[prop] = Math.max(this[prop] - hurt, 0);
 		},
 		getRandom(min, max) {
 			const value = Math.random() * (max - min) + min;
 			return Math.round(value);
 		},
+		heal(min, max) {
+			const heal = this.getRandom(min, max);
+			this.playerLife = Math.min(this.playerLife + heal, 100);
+		},
+		healAndHurt() {
+			this.heal(10, 15);
+			this.hurt('playerLife', 7, 12, false);
+		},
 	},
-	watch: {},
+	watch: {
+		hasResult(value) {
+			if (value) this.running = false;
+		},
+	},
 });
